4f0cb7c1e4e415ff57d4c69218608a9ebee20a03
Zachary Derrick <derrick.zachary@gmail.com>
2022-07-27 00:22:15 +0000

    My first bash script

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Zachary Derrick                    
+# Date of latest revision:  7/26/22    
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "I am working perfectly"
+# End
