70c04cc2e1c5cd5ca0cafeb648fc091d22515fa8
Zachary Derrick <derrick.zachary@gmail.com>
2022-07-28 02:32:45 +0000

    This was a script I wrote to give the login history using the terminal

---
+++ Funtions.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# Script:                       Ops 201 Class 03 Ops Challenge Function
+# Author:                       Zachary Derrick
+# Date of latest revision:      7/27/2022
+# Purpose:                      Write a script that prints the login history of users on this computer.
+
+#main
+function Login_History {
+         sudo last
+}
+
+#This calls Login_History
+Login_History
+
+#This will add text asking a question 
+Question="Anything Sketchy?"
+  
+echo $Question
+
+# The yes/no part of the code I found from https://stackoverflow.com/a/226724
+# This code gives the option to answer the question I posed above.
+# select yn is built into Bash's "select" command.
+# case and esac contain this part of the command.
+select yn in "Yes" "No" ; do
+    case $yn in
+        Yes ) echo "LULZ U'V BN H@XX'D"; break;;
+        No ) echo "(ɔ◔‿◔)ɔ ♥"; exit;;
+        
+    esac
+        
+done
+
+#end
