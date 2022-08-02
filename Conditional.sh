b21b1d825976dd3639a37b7008b06855e8155582
Zachary Derrick <derrick.zachary@gmail.com>
2022-08-02 15:54:06 +0000

    changed the array to include a timestamp.  added more comments.

--- Conditional.sh
+++ Conditional.sh
@@ -7,21 +7,25 @@
 # then creates it if it does not exist.
 
 #main
- useless=(This Array Is Useless RIP)
-#  file="useless.txt"
-    echo "Welcome to Junk File Creator"
-#   printf acts like echo but has the potential to be more functional 
-    printf "\n Please enter a file you'd like to find: "
-    read namefile
-    printf "\n Please enter the absolute path to see if it exists on this computer: "
-    read filepath
+ currentDate=`date` 
+ timestamp=(This file was created on $currentDate)
 
-if find $filepath -name $namefile -print -quit |
+    echo "Welcome to Junk File Creator"
+#printf acts like echo but has the potential to be more functional 
+    printf "\nPlease enter a file you'd like to find: "
+    read namefile
+    printf "\nPlease enter the absolute path to see if it exists on this computer: "
+    read filepath
+#below is a if then statement that looks for a file in a filepath. find searches for files in a directory hierarchy
+# -quit is to stop searching the file system once we have found what we want. find is used to search in directories
+# while grep returns files whose content matches the pattern you are looking for. grep -q allows grep to stop as soon as it finds the first match
+if find $filepath -name $namefile -quit |
    grep -q '^'; then
   echo "The file exists"
 else
   echo "Your file does not exist"
   echo "Would you like to create it?"
+#   select yn doesn't need you to sanitize the input.  it displays the choices and you pick the number.It loops automatically.
   select yn in "Yes" "No" ;
     do
         case $yn in
@@ -33,7 +37,7 @@ else
            
     done
 # This echo command to the Array was even more useless in an earlier version of the code.  
-    echo "${useless[*]}"
+    echo "${timestamp[*]}"
    
 fi
 
