ab0cda6942341eed1f66ff2f05d90a6fde0eb76d
Zachary Derrick <derrick.zachary@gmail.com>
2022-08-09 01:23:31 +0000

    commented out script so that it could be seen on Github

---
+++ SystemProcessCommands.sh
@@ -0,0 +1,9 @@
+#Get-Process | Sort-Object CPU -Descending
+#Get-Process | Sort-Object ID -Descending
+#Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 5
+#Start-Process -file iexplore 'https://owasp.org/www-project-top-ten/'
+#Start-Sleep -Seconds .5
+#for($i=0; $i -le 10; $i++) {Start-Process -file iexplore -arg 'https://owasp.org/www-project-top-ten/'}
+#Start-Sleep -Seconds 5 
+#Stop-Process -name msedge -Force
+#Stop-Process -id 8288 -Force
