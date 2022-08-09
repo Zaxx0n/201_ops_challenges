a39926e5de0e5ec7207af118c377519b26752281
Zachary Derrick <derrick.zachary@gmail.com>
2022-08-09 01:24:36 +0000

    commented out script so it can be read on Github

--- Automated Endpoint Configuration.ps1
+++ Automated Endpoint Configuration.ps1
@@ -1,17 +1,17 @@
-Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+# Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
 
-Start-Sleep -Seconds 1.5
+# Start-Sleep -Seconds 1.5
 
-netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
+# netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
 
-Start-Sleep -Seconds 1.5
+# Start-Sleep -Seconds 1.5
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+# iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
 
-Start-Sleep -Seconds 1.5
+# Start-Sleep -Seconds 1.5
 
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
-Start-Sleep -Seconds 1.5
+# Start-Sleep -Seconds 1.5
 
-Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+# Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
