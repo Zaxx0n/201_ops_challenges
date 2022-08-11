9fa8eb9bc8d69e713af0bf14c83eabe06b89b2d0
Zachary Derrick <derrick.zachary@gmail.com>
2022-08-11 05:29:39 +0000

    Added some text formatting and changed the wording of an echo response.

--- Domain_Analyzer.sh
+++ Domain_Analyzer.sh
@@ -5,6 +5,10 @@
 # Purpose: Create a script that asks a user to type a domain, then displays information about the typed domain.
 #Operations that must be used include "whois" "dig" "host" "nslookup"
 
+#text format 
+UWhite='\033[4;37m'       # White Underline
+Color_Off='\033[0m'       # Reset
+
 #define functions
 function dfun {
 
@@ -17,8 +21,8 @@ function dfun {
 }
 
 #main
-echo "Enter the url or IP Address of the domain you'd like information about *do not type 'www.':"
+echo -e "Enter the URL or IP Address of the domain you'd like information about. ${UWhite}Do not include the 'www'${Color_Off}:" && read ipurl
 #user enters a url to get information on
-read ipurl
+
 dfun
 #end
