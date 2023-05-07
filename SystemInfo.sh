a817b2223e04f02df1f4399f0bdda43293a1122c
Zachary Derrick <derrick.zachary@gmail.com>
2023-05-07 04:40:13 +0000

    Added a loop to the menu exiting with exit 0

--- SystemInfo.sh
+++ SystemInfo.sh
@@ -1,4 +1,4 @@
-#!/user/bin/bash
+#!/usr/bin/bash
 
 # Script: Ops 201 Class 07 Ops Challenge Solution
 # Author: Zachary Derrick                    
@@ -8,20 +8,26 @@
 
 #main
 
-  echo "Please Select the Hardware You'd Like More Information About"
+prompt="\nPlease select the hardware you'd like more information about:"
 
 # I turned a previous "select yn in" loop code into a selection tool for the System 
 # information that the user is looking for lshw is the list system hardware command
 # and -A Print NUM lines of trailing context after matching lines.
-  select yn in "Computer Name" "CPU" "RAM" "Display Adapter" "Network Adapter" ; do
+run_menu=0
+while [ $run_menu -eq 0 ]
+do
+  echo -e $prompt
+  select yn in "Computer Name" "CPU" "RAM" "Display Adapter" "Network Adapter" "Exit Program" ; do
         case $yn in
             "Computer Name" ) echo "Your computer name is:" && sudo lshw | grep -E '' | head -n1; break;;
             CPU ) sudo lshw | grep -A 6 '*-cpu'; break;;
             RAM ) sudo lshw | grep -A 3 '*-memory'; break;;
             "Display Adapter" ) sudo lshw | grep -A 12 '*-display'; break;;     
             "Network Adapter" ) sudo lshw | grep -A 15 '*-network'; break;;
+            "Exit Program" ) echo "Goodbye" && exit 0
         esac       
-    done
+  done
+done        
 # sudo lshw | grep ubuntudev    
 # sudo lshw -c system | grep -E 'product:'; break;;
 # echo "The Computer Name is: ubuntudev"
