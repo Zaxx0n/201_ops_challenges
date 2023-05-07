1062a51c3db042e3b403888d6a70c103b04071e1
Zachary Derrick <derrick.zachary@gmail.com>
2023-05-07 04:56:30 +0000

    fixed the 'purpose' section

--- SystemInfo.sh
+++ SystemInfo.sh
@@ -2,18 +2,17 @@
 
 # Script: Ops 201 Class 07 Ops Challenge Solution
 # Author: Zachary Derrick                    
-# Date of latest revision:  8/1/22    
-# Purpose: Create a script that detects if a file or directory exists, 
-# then creates it if it does not exist.
+# Date of latest revision:  2023/5/6    
+# Purpose:  # A script that lists hardware of the PC. I turned a previous "select yn in" loop code into a selection tool for the System 
+            # information that the user is looking for lshw is the list system hardware command
+            # and -A Print NUM lines of trailing context after matching lines.
 
-#main
 
+# declare variables
 prompt="\nPlease select the hardware you'd like more information about:"
-
-# I turned a previous "select yn in" loop code into a selection tool for the System 
-# information that the user is looking for lshw is the list system hardware command
-# and -A Print NUM lines of trailing context after matching lines.
 run_menu=0
+
+# main
 while [ $run_menu -eq 0 ]
 do
   echo -e $prompt
@@ -24,12 +23,9 @@ do
             RAM ) sudo lshw | grep -A 3 '*-memory'; break;;
             "Display Adapter" ) sudo lshw | grep -A 12 '*-display'; break;;     
             "Network Adapter" ) sudo lshw | grep -A 15 '*-network'; break;;
-            "Exit Program" ) echo "Goodbye" && exit 0
+            "Exit Program" ) echo "Goodbye" && echo "Your query was completed on" `date` && exit 0
         esac       
   done
 done        
-# sudo lshw | grep ubuntudev    
-# sudo lshw -c system | grep -E 'product:'; break;;
-# echo "The Computer Name is: ubuntudev"
-echo "Your query was completed on" `date`
+
 #end
